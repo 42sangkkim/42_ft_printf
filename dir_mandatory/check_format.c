d3884611f0d8426e4af69fb380a1d918880e2992
sangkkim <madlogic365@gmail.com>
2022-02-10 12:18:34 +0900

    [get_format.c] [check_format.c] downgrade for mandatory

--- dir_mandatory/check_format.c
+++ dir_mandatory/check_format.c
@@ -6,7 +6,7 @@
 /*   By: sangkkim <sangkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 21:24:42 by sangkkim          #+#    #+#             */
-/*   Updated: 2022/02/10 12:06:58 by sangkkim         ###   ########.fr       */
+/*   Updated: 2022/02/10 12:17:04 by sangkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@
 #define INT_MAX 0x7FFFFFFF
 
 int	valid_specifier(char specifier);
-int	valid_flags(char specifier, t_flags flags);
-int	valid_width_precision(long width, long precision);
 
 int	check_format(t_format format)
 {
 	if (!valid_specifier(format.specifier))
-		return (0);
-	else if (!valid_flags(format.specifier, format.flags))
-		return (0);
-	else if (!valid_width_precision(format.width, format.precisoin))
 		return (0);
 	else
 		return (1);
@@ -34,35 +28,4 @@ int	check_format(t_format format)
 int	valid_specifier(char specifier)
 {
 	return (strchr("cspdiuxX%", specifier) > 0);
-}
-
-// [ + sp # * .* 0 - ]
-int	valid_flags(char specifier, t_flags flags)
-{
-	if (flags.minus && flags.zero)
-		return (0);
-	else if (flags.space && flags.plus)
-		return (0);
-	if (specifier == 'c' || specifier == 'p')
-		return (!(flags.byte & 0b1110110));
-	else if (specifier == 's')
-		return (!(flags.byte & 0b1110010));
-	else if (specifier == 'd' || specifier == 'i')
-		return (!(flags.byte & 0b0010000));
-	else if (specifier == 'u')
-		return (!(flags.byte & 0b1110000));
-	else if (specifier == 'x' || specifier == 'x')
-		return (!(flags.byte & 0b1100000));
-	else
-		return (1);
-}
-
-int	valid_width_precision(long width, long precision)
-{
-	if (width < 0 || width > INT_MAX)
-		return (0);
-	else if (precision < 0 || precision > INT_MAX)
-		return (0);
-	else
-		return (1);
 }
--- dir_mandatory/get_format.c
+++ dir_mandatory/get_format.c
@@ -6,16 +6,12 @@
 /*   By: sangkkim <sangkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 20:50:08 by sangkkim          #+#    #+#             */
-/*   Updated: 2022/02/10 12:06:31 by sangkkim         ###   ########.fr       */
+/*   Updated: 2022/02/10 12:17:37 by sangkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/format.h"
 #include "../ft_printf.h"
-
-void	set_flags(const char **f_string, t_format *format);
-void	set_width(const char **f_string, t_format *format);
-void	set_precision(const char **f_string, t_format *format);
 
 t_format	get_format(const char **f_string)
 {
@@ -23,50 +19,8 @@ t_format	get_format(const char **f_string)
 
 	bzero(&format, sizeof(t_format));
 	*f_string += 1;
-	set_flags(f_string, &format);
-	if (isdigit(**f_string))
-		set_width(f_string, &format);
-	if (**f_string == '.')
-		set_precision(f_string, &format);
 	format.specifier = **f_string;
 	if (**f_string)
 		*f_string += 1;
 	return (format);
-}
-
-void	set_flags(const char **f_string, t_format *format)
-{
-	while (1)
-	{
-		if (**f_string == '-')
-			format->flags.minus = 1;
-		else if (**f_string == '0')
-			format->flags.zero = 1;
-		else if (**f_string == '#')
-			format->flags.sharp = 1;
-		else if (**f_string == ' ')
-			format->flags.space = 1;
-		else if (**f_string == '+')
-			format->flags.plus = 1;
-		else
-			break ;
-		*f_string += 1;
-	}
-}
-
-void	set_width(const char **f_string, t_format *format)
-{
-	format->flags.width = 1;
-	format->width = strtol(*f_string, NULL, 10);
-	while (isdigit(**f_string))
-		*f_string += 1;
-}
-
-void	set_precision(const char **f_string, t_format *format)
-{
-	format->flags.precision = 1;
-	*f_string += 1;
-	format->precisoin = strtol(*f_string, NULL, 10);
-	while (isdigit(**f_string))
-		*f_string += 1;
 }
